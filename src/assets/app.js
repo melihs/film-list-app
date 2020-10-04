256700de00110ecf7b48cbe0d98703a5b730ce45
melih şahin <melihsahin24@gmail.com>
2020-10-04 19:48:12 +0300

    Add favorite film

--- src/assets/app.js
+++ src/assets/app.js
@@ -1,5 +1,8 @@
 let searchController = {
     init: function () {
+        this.defaultImage = "./assets/img/no-image.jpg";
+        this.key = "7ddb68cb&s";
+        this.url = "http://www.omdbapi.com/?i=tt3896198&";
     },
     onload: function () {
         this.doms = {
@@ -11,24 +14,25 @@ let searchController = {
     },
     bindActions: function () {
         formSubmit(this.doms.searchForm, this.doms.searchText, this.doms.elmSearchResult);
+        filmCard();
     },
     functions: {
         getMovies: getMovies = (searchText, elmSearchResult) => {
-            key = "7ddb68cb&s=",
-                url = "http://www.omdbapi.com/?i=tt3896198&";
-
-            fetch(url + "apikey=" + key + searchText.val() + "&format=json")
+            fetch(`${searchController.url}apikey=${searchController.key}=${searchText.val()}&format=json`)
                 .then(response => response.json())
                 .then(data => {
                     let movies = data.Search;
+
                     elmSearchResult.empty();
 
                     $.each(movies, (index, movie) => {
-                        let image = movie.Poster == 'N/A' ? "./assets/img/no-image.jpg" : movie.Poster;
+                        image = movie.Poster == 'N/A' ? searchController.defaultImage : movie.Poster;
 
-                        output = filmCard(image, movie.Title, movie.Year);
+                        output = filmCard(image, movie.Title, movie.Year, movie.imdbID);
 
                         elmSearchResult.append(output);
+
+                        setFavorite(movie.imdbID);
                     });
                 })
                 .catch(err => console.error(err));
@@ -40,7 +44,7 @@ let searchController = {
                 e.preventDefault();
             })
         },
-        filmCard: filmCard = (imageSource, cardTitleText, releaseDate) => {
+        filmCard: filmCard = (imageSource, cardTitleText, releaseDate, filmId) => {
             const elmCardDiv = $("<div class='col-md-4 mb-4'>").css("display", "flex");
 
             const elmCard = $("<div class='card'>").appendTo(elmCardDiv);
@@ -65,12 +69,15 @@ let searchController = {
 
             const elmSpanFav = $("<small class='text-muted float-right'>").appendTo(elmCardFooter);
 
-            const elmIcon = $("<i class='far fa-heart fa-2x'>").appendTo(elmSpanFav);
+            const elmIcon = $("<i class='far fa-heart fa-2x'>").attr('id', `${filmId}`).css('cursor', 'pointer').appendTo(elmSpanFav);
 
             return elmCardDiv[0];
         },
-        searchHistory: searchHistory = (searchText) => {
 
+        setFavorite: setFavorite = (filmId) => {
+            $(`#${filmId}`).on('click', () => {
+                $(`#${filmId}`).css('color', 'red').addClass('fas').removeClass('far');
+            });
         }
     }
 }
